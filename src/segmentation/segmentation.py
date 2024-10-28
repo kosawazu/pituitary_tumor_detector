114e8792eb90c20e5f7f5dcfd3f4fd958d9ccab1
s-kosawazu <s-kosawazu.@sic-net.co.jp>
2024-10-28 09:30:15 +0900

    resizeの画像サイズを適切な値に変更

--- src/segmentation/segmentation.py
+++ src/segmentation/segmentation.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # fcn_resnet50用
 transform = transforms.Compose([
-    transforms.Resize((520, 520)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
