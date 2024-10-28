6fdfe9c78c3d48618e3a571188a477d17b3d7012
s-kosawazu <s-kosawazu.@sic-net.co.jp>
2024-10-28 09:28:48 +0900

    bashの更新

--- src/segmentation/segment.sh
+++ src/segmentation/segment.sh
@@ -1,4 +1,3 @@
-
 model_name="fcn_resnet101"
 epochs=50
 batch_size=64
@@ -7,4 +6,4 @@ class_num=3
 
 python3 train_fcn.py --epochs "$epochs" --model_name "$model_name" --batch_size "$batch_size" --lerning_rate "$lerning_rate" --class_num "$class_num"
 
-python3 test_fcn.py --model_name "$model_name" --class_num "$class_num"
+python3 test_fcn.py --model_name "$model_name" --class_num "$class_num" --batch_size "$batch_size" --lerning_rate "$lerning_rate"
