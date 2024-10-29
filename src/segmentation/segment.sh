84829636e21ee78e194807b5016967ebac5652b4
s-kosawazu <s-kosawazu.@sic-net.co.jp>
2024-10-29 16:28:13 +0900

    引数などを少し変更

--- src/segmentation/segment.sh
+++ src/segmentation/segment.sh
@@ -3,10 +3,12 @@ model_name="$1"
 epochs="$2"
 batch_size="$3"
 learning_rate="$4"
-class_num="$5"
+patience="$5"
 
 # トレーニングの実行
-python3 train_fcn.py --epochs "$epochs" --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate" --class_num "$class_num"
+python3 train_fcn.py --epochs "$epochs" --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate" --patience "$patience"
 
 # テストの実行
-python3 test_fcn.py --model_name "$model_name" --class_num "$class_num" --batch_size "$batch_size" --learning_rate "$learning_rate"
+python3 test_fcn.py --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate"
+
+echo "全ての推論と学習が終了しました"
