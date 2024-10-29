d1c09d58fe9151ce06215ddf9eec667a083d9867
s-kosawazu <s-kosawazu.@sic-net.co.jp>
2024-10-29 15:27:23 +0900

    コマンドラインから値を指定するように修正した

--- src/segmentation/segment.sh
+++ src/segmentation/segment.sh
@@ -1,9 +1,12 @@
-model_name="fcn_resnet101"
-epochs=50
-batch_size=64
-lerning_rate=1e-4
-class_num=3
+# コマンドライン引数を変数に格納
+model_name="$1"
+epochs="$2"
+batch_size="$3"
+learning_rate="$4"
+class_num="$5"
 
-python3 train_fcn.py --epochs "$epochs" --model_name "$model_name" --batch_size "$batch_size" --lerning_rate "$lerning_rate" --class_num "$class_num"
+# トレーニングの実行
+python3 train_fcn.py --epochs "$epochs" --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate" --class_num "$class_num"
 
-python3 test_fcn.py --model_name "$model_name" --class_num "$class_num" --batch_size "$batch_size" --lerning_rate "$lerning_rate"
+# テストの実行
+python3 test_fcn.py --model_name "$model_name" --class_num "$class_num" --batch_size "$batch_size" --learning_rate "$learning_rate"
