5537de088d61d0d6b2e09c8008b60863e19ec523
s-kosawazu <s-kosawazu.@sic-net.co.jp>
2024-10-31 10:08:15 +0900

    パラメータ探索をできるようにした

--- src/segmentation/segment.sh
+++ src/segmentation/segment.sh
@@ -1,14 +1,29 @@
-# コマンドライン引数を変数に格納
 model_name="$1"
 epochs="$2"
-batch_size="$3"
-learning_rate="$4"
+batch_size_list=($3)  # リストとして展開
+learning_rate_list=($4)  # リストとして展開 
 patience="$5"
 
-# トレーニングの実行
-python3 train_fcn.py --epochs "$epochs" --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate" --patience "$patience"
+# バッチサイズと学習率のリストをループで回す
+for batch_size in "${batch_size_list[@]}"; do
+  for learning_rate in "${learning_rate_list[@]}"; do
+    echo "バッチサイズ: $batch_size, 学習率: $learning_rate"
 
-# テストの実行
-python3 test_fcn.py --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate"
+    # フォルダのパスを定義
+    result_folder="../../result/nagoya/training_results/${model_name}/${batch_size}/${learning_rate}"
+
+    # フォルダが存在する場合はスキップ
+    if [ -d "$result_folder" ]; then
+      echo "フォルダ $result_folder が既に存在します。次の処理に進みます。"
+      continue
+    fi
+
+    # トレーニングの実行
+    python3 train_fcn.py --epochs "$epochs" --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate" --patience "$patience"
+
+    # テストの実行
+    python3 test_fcn.py --model_name "$model_name" --batch_size "$batch_size" --learning_rate "$learning_rate"
+  done
+done
 
 echo "全ての推論と学習が終了しました"
